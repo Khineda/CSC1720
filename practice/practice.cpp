191ca77ba707f6c01ff1d1d7a043aea01f2c9b77
Khineda <kvw0303@gmail.com>
2025-04-03 10:01:55 -0400

    commmit

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,35 +1,18 @@
-
-
 #include <iostream>
-#include <string>
-#include <iomanip>
-
 using namespace std;
 
+int rFibNum(int a, int b, int n) {
+	if (n ==1) 
+	return a;
 
-template <class type>
-type funcExp(type list[], int size)
-{
-type x = list[0];
-type y = list[size - 1];
-for (int j = 1; j < size - 1; j++)
-{
-if (x < list[j])
-x = list[j];
-if (y > list[size - 1 - j])
-y = list[size - 1 - j];
+	else if (n == 2) 
+	return b;
+
+	else 
+	return rFibNum(a, b, n - 1) + rFibNum(a, b, n - 2);
 }
-return x;
-}
-
-double sales[7] = {280.50, 320.00, 56.00, 78.90, 300.00,100.00, 250.00};
-
-string names[] = {"Mike", "Lisa", "Nancy", "Robinson","Miller", "Sam"};
 
 int main() {
-	cout << funcExp(sales, 7) << endl;
-
-	cout << funcExp(names, 6) << endl;
-
-	return 0;
+	int fib = rFibNum(1, 2, 5);
+	cout << fib << endl;
 }

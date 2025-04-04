9b52d764158e90970ac28f443eaae2e9a86cd6bd
Khineda <kvw0303@gmail.com>
2025-04-04 11:56:34 -0400

    ``` Add initial project setup ```

---
+++ projectFinal/main.cpp
@@ -0,0 +1,30 @@
+/********************************************************************** 
+
+* File Name: main.cpp
+
+* Programmer: Kinsey Van Wingerden
+
+* Course: CSC1720 Section 2
+
+* Date: 3/23/2025
+
+* Path: /home/students/jvanwing/CSC1720/projectFinal/main.cpp
+
+* Description: This is the temporary main file for the final project
+
+* Additional Files: products.txt
+
+* Help Recieved: 
+
+* Compile: g++ main.cpp
+
+* Run: ./a.out
+
+***********************************************************************/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
